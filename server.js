87f82ae85eb40c49810cc1d63130cf15de0e63f1
hsu-patty <pattyhsu615@gmail.com>
2024-09-24 13:29:09 -0700

    Add server listening

--- server.js
+++ server.js
@@ -2,45 +2,45 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const { getGreeting } = require("./modules/utils");
-const PORT = process.env.PORT || 25678;
+const PORT = process.env.PORT || 5002;
 
-http
-  .createServer((req, res) => {
-    let urlString = url.parse(req.url, true);
-    let path = urlString.pathname;
+const server = http.createServer((req, res) => {
+  let urlString = url.parse(req.url, true);
+  let path = urlString.pathname;
 
-    if (path === "/COMP4537/labs/3/getDate/") {
-      let name = urlString.query["name"];
-      let message = getGreeting(name);
+  if (path === "/COMP4537/labs/3/getDate/") {
+    let name = urlString.query["name"];
+    let message = getGreeting(name);
 
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(`<div style="color: blue;">${message}</div>`);
-    } else if (path === "/COMP4537/labs/3/writeFile/") {
-      let text = urlString.query["text"];
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<div style="color: blue;">${message}</div>`);
+  } else if (path === "/COMP4537/labs/3/writeFile/") {
+    let text = urlString.query["text"];
 
-      fs.appendFile("file.txt", text + "\n", (err) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/html" });
-          res.end("Error writing to file");
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end("Text appended to file");
-        }
-      });
-    } else if (path.startsWith("/COMP4537/labs/3/readFile/")) {
-      let fileName = path.split("/").pop();
+    fs.appendFile("file.txt", text + "\n", (err) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Error writing to file");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Text appended to file");
+      }
+    });
+  } else if (path.startsWith("/COMP4537/labs/3/readFile/")) {
+    let fileName = path.split("/").pop();
 
-      console.log(fileName);
-      fs.readFile(fileName, (err, data) => {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end(fileName + "404 Not Found");
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-        }
-      });
-    }
-  })
-  .listen(PORT);
+    console.log(fileName);
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end(fileName + "404 Not Found");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      }
+    });
+  }
+});
+
+server.listen(PORT);
